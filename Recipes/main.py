41d34e3cde85dd6bad82f2cd62b1f3042e47142c
jorgegarcelan <jorgegarcelan@gmail.com>
2023-11-07 17:06:39 +0100

    added user.html

--- Recipes/main.py
+++ Recipes/main.py
@@ -8,3 +8,7 @@ bp = Blueprint("main", __name__)
 @bp.route("/")
 def index():
     return render_template("main/main.html")
+
+@bp.route("/user")
+def user():
+    return render_template("user/user.html")
