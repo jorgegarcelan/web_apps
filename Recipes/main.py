ad7934ecd8148db807be3bd76258134a75e99415
jorgegarcelan <jorgegarcelan@gmail.com>
2023-11-22 16:54:35 +0100

    updated recipes view in main, css, html

--- Recipes/main.py
+++ Recipes/main.py
@@ -1,6 +1,6 @@
 import datetime
 import dateutil.tz
-from flask import Blueprint, render_template, abort, request, url_for
+from flask import Blueprint, render_template, abort, request, url_for, flash
 import flask_login
 from sqlalchemy.sql import func
 from . import model
@@ -48,7 +48,6 @@ def recipe(recipe_id):
     query_r = db.select(model.Recipe).where(model.Recipe.id == recipe_id)
     recipe = db.session.execute(query_r).scalar_one_or_none()
     print(recipe)
-
     
     # user:
     query_u = db.select(model.User).where(model.User.id == recipe.user_id)
@@ -58,13 +57,63 @@ def recipe(recipe_id):
     # rating:
     query_rt = db.select(model.Rating.value).where(model.Rating.recipe_id == recipe_id)
     ratings_list = db.session.execute(query_rt).scalars().all()
-    rating = np.round(np.mean(ratings_list), 1)
-    print(rating is np.nan)
-    if rating == 0: ### NO FUNCIONA
-        rating = "No reviews yet"
+    count = len(ratings_list)
 
+    # steps: ### ordenar por seuqence
+    print(recipe.steps)
+    steps = recipe.steps
+
+    # ingredients:
+    print(recipe.quantified_ingredients)
+    # Query the QuantifiedIngredient model to get all entries related to the recipe_id
+    quantified_ingredients = model.QuantifiedIngredient.query.filter_by(recipe_id=recipe_id).all()
+
+    # for each quantified ingredient, retrieve the ingredient details
+    ingredients_info = []
+    for qi in quantified_ingredients:
+        ingredient_detail = {
+            'name': qi.ingredient.name,
+            'quantity': qi.quantity,
+            'unit_of_measurement': qi.unit_of_measurement
+        }
+        ingredients_info.append(ingredient_detail)
+
+    # bookmark:
+    """
+     # Check if the current user has already bookmarked the item
+    query_b = model.Bookmark.query.filter_by(
+                                        user_id=user.id,
+                                        recipe_id=recipe.id
+                                    ).first()
+
+    if query_b:
+        # The user has already bookmarked this item
+        flash('You have already bookmarked this item.', 'info')
+    else:
+        # The user has not bookmarked this item, create a new bookmark
+        new_bookmark = model.Bookmark(user_id=user.id, recipe_id=recipe.id)
+        db.session.add(new_bookmark)
+        try:
+            db.session.commit()
+            flash('Item bookmarked successfully!', 'success')
+        except Exception as e:
+            db.session.rollback()
+            flash('An error occurred while bookmarking the item.', 'error')
+    """
     
-    return render_template("recipes/recipes.html", recipe=recipe, user=user, rating=rating)
+
+    if count == 0:
+        rating = "No reviews yet"
+        return render_template("recipes/recipes.html", recipe=recipe, user=user, rating=rating, ingredients_info=ingredients_info)
+
+    else:
+        count = f"({count})"
+        rating = round(np.mean(ratings_list), 1)
+        rating = str(rating) + " / 5"
+        return render_template("recipes/recipes.html", recipe=recipe, user=user, rating=rating, count=count, ingredients_info=ingredients_info)
+
+
+
 
 
 @bp.route("/recipe_vision", methods=['GET', 'POST'])
