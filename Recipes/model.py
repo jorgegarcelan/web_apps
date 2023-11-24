1768434a04db142d96793e956e22c235c7a5b83a
jorgegarcelan <jorgegarcelan@gmail.com>
2023-11-24 15:24:19 +0100

    added recipe relationship to bookmark

--- Recipes/model.py
+++ Recipes/model.py
@@ -71,6 +71,7 @@ class Bookmark(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+    recipe = db.relationship('Recipe', backref='bookmarks')
 
 
 class Photo(db.Model):
